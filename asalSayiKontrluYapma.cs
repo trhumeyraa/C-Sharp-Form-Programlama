653adec5a0abc4759c6a07860aefba4f79065330
Humeyra Goray <120178302+trhumeyraa@users.noreply.github.com>
2024-03-11 16:42:34 +0300

    Add files via upload

---
+++ asalSayiKontrluYapma.cs
@@ -0,0 +1,32 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int kontrol = 0;
+            for(int i = 1; i<sayi1; i++)
+            {
+                if (sayi1 % i == 0)
+                {
+                    kontrol++;
+                }
+            }
+            if (kontrol !=0)
+            {
+                MessageBox.Show("deðil");
+            }
+            else
+            {
+                MessageBox.Show("asal");
+            }
+            }
+        }
+    }
+
